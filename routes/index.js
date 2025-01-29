9337474a3d875e7235069b1b4b6913391624505c
Esdras Vidal Pereira <esdras015@gmail.com>
2025-01-29 11:05:14 -0300

    Add initial config for socket.io

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 module.exports = router;
